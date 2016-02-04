03b881ec7c82e4f03ee8ed971a9305dc3cc61112
Balogh Gergõ <geryxyz@inf.u-szeged.hu>
2016-02-04 14:37:35 +0100

    first working version

---
+++ ColorLens/ColorLens/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorLens
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow( )
+		{
+			InitializeComponent( );
+		}
+
+		void MainWindow_OnLoaded( object sender, RoutedEventArgs e )
+		{
+			new TaskFactory( ).StartNew(
+				( ) =>
+				{
+					while ( true )
+					{
+						var _position = Win32.CursorPosition;
+						var _color = Win32.GetPixelColor( _position.X, _position.Y );
+						Dispatcher.Invoke(
+							( ) =>
+							{
+								rgbDisplay.Text = string.Format( "{0}; {1}; {2};", _color.R, _color.G, _color.B );
+								colorDisplay.Fill = new SolidColorBrush( Color.FromRgb( _color.R, _color.G, _color.B ) );
+								Top = _position.Y + 10;
+								Left = _position.X + 10;
+							} );
+						Thread.Sleep( 10 );
+					}
+				} );
+		}
+	}
+}
---
+++ ColorLens/ColorLens/Win32.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace ColorLens
+{
+	internal sealed class Win32
+	{
+		[ DllImport( "user32.dll" ) ]
+		static extern IntPtr GetDC( IntPtr hwnd );
+
+		[ DllImport( "user32.dll" ) ]
+		static extern Int32 ReleaseDC( IntPtr hwnd, IntPtr hdc );
+
+		[ DllImport( "gdi32.dll" ) ]
+		static extern uint GetPixel( IntPtr hdc, int nXPos, int nYPos );
+
+		public static System.Drawing.Color GetPixelColor( int x, int y )
+		{
+			IntPtr hdc = GetDC( IntPtr.Zero );
+			uint pixel = GetPixel( hdc, x, y );
+			ReleaseDC( IntPtr.Zero, hdc );
+			Color color = Color.FromArgb(
+				( int )( pixel & 0x000000FF ),
+				( int )( pixel & 0x0000FF00 ) >> 8,
+				( int )( pixel & 0x00FF0000 ) >> 16 );
+			return color;
+		}
+
+		[ StructLayout( LayoutKind.Sequential ) ]
+		public struct POINT
+		{
+			public int X;
+			public int Y;
+
+			public static implicit operator Point( POINT point )
+			{
+				return new Point( point.X, point.Y );
+			}
+		}
+
+		[ DllImport( "user32.dll" ) ]
+		public static extern bool GetCursorPos( out POINT lpPoint );
+
+		public static Point CursorPosition
+		{
+			get
+			{
+				POINT lpPoint;
+				GetCursorPos( out lpPoint );
+				return lpPoint;
+			}
+		}
+	}
+}
