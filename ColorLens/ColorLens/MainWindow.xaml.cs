8e5ca6b8645747c14167563d60009b640792bda3
Gergő Balogh <geryxyz@gmail.com>
2019-10-01 12:26:58 +0200

    back-up

--- ColorLens/ColorLens/MainWindow.xaml.cs
+++ ColorLens/ColorLens/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,9 +11,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
 
 namespace ColorLens
 {
@@ -24,7 +27,74 @@ namespace ColorLens
 		public MainWindow( )
 		{
 			InitializeComponent( );
+			KeyDown +=
+				( _s, _a ) =>
+				{
+					if ( _a.Key == Key.LeftCtrl ||
+					     _a.Key == Key.RightCtrl )
+					{
+						isMoving = false;
+						helpDisplayer.Visibility = Visibility.Visible;
+					}
+					if ( _a.Key == Key.Escape )
+					{
+						Close( );
+					}
+					if ( _a.Key == Key.R )
+					{
+						Clipboard.SetText( color.R.ToString( ) );
+						ColorAnimation _fade =
+							new ColorAnimation( Colors.Transparent, Colors.Red, new Duration( TimeSpan.FromSeconds( .25 ) ) )
+							{
+								AutoReverse = true,
+							};
+						rgbBrush.BeginAnimation( SolidColorBrush.ColorProperty, _fade );
+					}
+					if ( _a.Key == Key.G )
+					{
+						Clipboard.SetText( color.G.ToString( ) );
+						ColorAnimation _fade =
+							new ColorAnimation( Colors.Transparent, Colors.Green, new Duration( TimeSpan.FromSeconds(.25 ) ) )
+							{
+								AutoReverse = true
+							};
+						rgbBrush.BeginAnimation( SolidColorBrush.ColorProperty, _fade );
+					}
+					if ( _a.Key == Key.B )
+					{
+						Clipboard.SetText( color.B.ToString( ) );
+						ColorAnimation _fade =
+							new ColorAnimation( Colors.Transparent, Colors.Blue, new Duration( TimeSpan.FromSeconds( .25 ) ) )
+							{
+								AutoReverse = true
+							};
+						rgbBrush.BeginAnimation( SolidColorBrush.ColorProperty, _fade );
+					}
+					if ( _a.Key == Key.H )
+					{
+						Clipboard.SetText( ColorTranslator.ToHtml( color ) );
+						ColorAnimation _fade =
+							new ColorAnimation( Colors.Transparent, Colors.Black, new Duration( TimeSpan.FromSeconds( .25 ) ) )
+							{
+								AutoReverse = true
+							};
+						htmlBrush.BeginAnimation( SolidColorBrush.ColorProperty, _fade );
+					}					
+				};
+			KeyUp += 
+				( _s, _a ) =>
+				{
+					if ( _a.Key == Key.LeftCtrl ||
+					     _a.Key == Key.RightCtrl )
+					{
+						isMoving = true;
+						helpDisplayer.Visibility = Visibility.Collapsed;
+					}
+				};
 		}
+
+		bool isMoving = true;
+		System.Drawing.Color color;
 
 		void MainWindow_OnLoaded( object sender, RoutedEventArgs e )
 		{
@@ -33,16 +103,20 @@ namespace ColorLens
 				{
 					while ( true )
 					{
-						var _position = Win32.CursorPosition;
-						var _color = Win32.GetPixelColor( _position.X, _position.Y );
-						Dispatcher.Invoke(
-							( ) =>
-							{
-								rgbDisplay.Text = string.Format( "{0}; {1}; {2};", _color.R, _color.G, _color.B );
-								colorDisplay.Fill = new SolidColorBrush( Color.FromRgb( _color.R, _color.G, _color.B ) );
-								Top = _position.Y + 10;
-								Left = _position.X + 10;
-							} );
+						if ( isMoving )
+						{
+							var _position = Win32.CursorPosition;
+							color = Win32.GetPixelColor( _position.X, _position.Y );
+							Dispatcher.Invoke(
+								( ) =>
+								{
+									rgbDisplay.Text = string.Format( "{0}; {1}; {2};", color.R, color.G, color.B );
+									htmlDisplay.Text = ColorTranslator.ToHtml( color );
+									colorDisplay.Fill = new SolidColorBrush( Color.FromRgb( color.R, color.G, color.B ) );
+									Top = _position.Y - ( ActualHeight + 10 );
+									Left = _position.X + 10;
+								} );
+						}
 						Thread.Sleep( 10 );
 					}
 				} );
